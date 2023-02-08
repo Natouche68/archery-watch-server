f02b27f1545f86d982f9b29e76f6c7206d723b3f
Natouche68 <ngrossheny@gmail.com>
2023-02-08 19:20:39 +0100

    Make the logic for the stopwatches

--- app.js
+++ app.js
@@ -16,12 +16,24 @@ io.on('connection', (socket) => {
 
 	socket.on('start-stopwatch', (stopwatchId, twoWaves, time) => {
 		console.log('start-stopwatch =>', stopwatchId, twoWaves, time);
-		io.to('controllers').emit('stopwatch-started', stopwatchId);
+		io.to('stopwatches').emit('start-stopwatch', stopwatchId, twoWaves, time);
 	});
 
 	socket.on('stop-stopwatch', (stopwatchId) => {
 		console.log('stop-stopwatch =>', stopwatchId);
-		const waveTwoRunning = false; // PLACEHOLDER
+		io.to('stopwatches').emit('stop-stopwatch', stopwatchId);
+	});
+
+	socket.on('stopwatch-connected', () => {
+		socket.join('stopwatches');
+		console.log('stopwatch-connected');
+	});
+
+	socket.on('stopwatch-started', (stopwatchId) => {
+		io.to('controllers').emit('stopwatch-started', stopwatchId);
+	});
+
+	socket.on('stopwatch-stopped', (stopwatchId, waveTwoRunning) => {
 		io.to('controllers').emit('stopwatch-stopped', stopwatchId, waveTwoRunning);
 	});
 });
