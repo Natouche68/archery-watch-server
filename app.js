a2eae3eaa755dfd3908d08ad6a5635111765b1f2
Natouche68 <ngrossheny@gmail.com>
2023-02-08 18:48:08 +0100

    Make the logic for the controller

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ const io = new Server(httpServer, {
 });
 
 io.on('connection', (socket) => {
-	console.log(socket.id);
+	socket.on('controller-connected', (stopwatchId) => {
+		socket.join('controllers');
+		console.log('controller-connected =>', stopwatchId);
+	});
+
+	socket.on('start-stopwatch', (stopwatchId, twoWaves, time) => {
+		console.log('start-stopwatch =>', stopwatchId, twoWaves, time);
+		io.to('controllers').emit('stopwatch-started', stopwatchId);
+	});
+
+	socket.on('stop-stopwatch', (stopwatchId) => {
+		console.log('stop-stopwatch =>', stopwatchId);
+		const waveTwoRunning = false; // PLACEHOLDER
+		io.to('controllers').emit('stopwatch-stopped', stopwatchId, waveTwoRunning);
+	});
 });
 
 io.listen('8080');
